3688f6a65cdda305c5122897471cbb6203169e75
Indrajuni96 <indrajuniyanto96@gmail.com>
2019-11-24 08:54:45 +0700

    added cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const apiRoutes = require('./src/index')
+const cors = require('cors')
 
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 app.use('/api/v1', apiRoutes)
 
 app.listen(port, () => { console.log(`Service Runnning Using Port : ${port}`) })
