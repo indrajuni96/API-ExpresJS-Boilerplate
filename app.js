5ce907fd36f20176799d66323892052a886cbb08
Indrajuni96 <indrajuniyanto96@gmail.com>
2019-11-24 09:09:58 +0700

    added morgan(logger)

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const apiRoutes = require('./src/index')
 const cors = require('cors')
+const logger = require('morgan')
 
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use(logger('dev'))
 app.use('/api/v1', apiRoutes)
 
 app.listen(port, () => { console.log(`Service Runnning Using Port : ${port}`) })
