a4ee25fcb9124150a40042c401843e9e18895603
Indrajuni96 <indrajuniyanto96@gmail.com>
2019-11-27 10:07:37 +0700

    verift token

--- src/routes/example.js
+++ src/routes/example.js
@@ -2,9 +2,10 @@ const express = require('express')
 const route = express.Router()
 
 const exampleController = require('../controllers/example')
+const authMiddleware = require('../middleware/verifyToken')
 
 route
-    .get('/', exampleController.getExample)
+    .get('/', authMiddleware.isAuth, exampleController.getExample)
     .post('/create', exampleController.addExample)
     .put('/update/:id', exampleController.editExample)
     .delete('/delete/:id', exampleController.deleteExample)
